066660f5a229829373fb7b32e1dc882699df82cb
Taylor <taymarcel@gmail.com>
2016-04-20 20:57:02 +0000

    in class

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+#The basic Quote data model should have the following fields:
+#
+#author:string
+#
+#quote:text
+#
+#background:text
+#
+Quote.create(author:'Arthur C. Clarke',quote:'Any sufficiently advanced technology is indistinguisable from magic.',background:'Arthur C. Clarke')
+Quote.create(author:'Elon Musk',quote:'With artificial intelligence, we are summoning the demon.',background:'Now he makes electric cars.')
+Quote.create(author:'Pablo Picasso',quote:'Computers are useless. They can only give you answers.',background:'Some remarkable cubist paintings he did.')
